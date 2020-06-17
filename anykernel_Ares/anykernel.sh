979d6f06d5c31a326915d9302683d7b36f1a4586
Smg <smgkhoarn@gmail.com>
2020-06-17 15:18:56 +0900

    anykernel_Ares: Use blazko AK3 in Ares-9

--- anykernel_Ares/anykernel.sh
+++ anykernel_Ares/anykernel.sh
@@ -5,13 +5,13 @@
 #
 # Modified by The~Skater~187 @ xda-developers
 #
-# Repacked to AnyKernel3 by SmgKhOaRn @ xda-developers
+# Repacked to AnyKernel3 by @Blazko381
 #
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Ares Kernel by @The~Skater~187, repacked to AK3 by @SmgKhOaRn
+kernel.string=Ares Kernel by @The~Skater~187, repacked to AK3 by @Blazko381 & @SmgKhOaRn
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -59,69 +59,61 @@ dump_boot;
 
 # Check Android version
 ui_print " ";
-
-android_sdk=$(cat /system/build.prop | grep ro.build.version.sdk | cut -d "=" -f 2)
-case "$android_sdk" in
-26|27|28|29) support_status="supported";;
+ui_print "Checking android version...";
+android_ver=$(file_getprop /system/build.prop "ro.build.version.release");
+ui_print " ";
+ui_print "Android $android_ver detected...";
+case "$android_ver" in
+8.1.0|9) support_status="supported";;
   *) support_status="unsupported";;
 esac;
-
-if [ "$support_status" == "supported" ]; then
-	ui_print "Android 8.0/8.1/9.0/10 detected!";
-else
- ui_print "This version of Ares-Kernel is only compatible with android versions 8 & 8.1 & 9 & 10!";
- exit 1;
+ui_print " ";
+if [ ! "$support_status" == "supported" ]; then
+  ui_print "This version of Ares-Kernel is only compatible with android versions 8.1.0 & 9!";
+  exit 1;
 fi;
 
 dump_boot;
 
 # begin ramdisk changes
-mount -o rw,remount /system;
-mount -o rw,remount /system_root;
-
-# sysinit
-backup_file /system/bin/sysinit;
-backup_file /system/xbin/sysinit;
-replace_file /system/bin/sysinit 755 aressysinit
-replace_file /system/xbin/sysinit 755 aressysinit
 
 # init.rc
 backup_file init.rc;
-grep "import /init.Ares.rc" init.rc >/dev/null || sed -i '1,/.*import.*/s/.*import.*/import \/init.Ares.rc\n&/' init.rc;
-
-# init_d.rc
-backup_file /system/etc/init/init_d.rc;
-replace_file /system/etc/init/init_d.rc 755 init_d.rc
+grep "import /init.Ares.rc" init.rc >/dev/null || sed -i '1,/.*import.*/s/.*import.*/import \/init.Ares.rc\n&/' init.rc
 
 # init.qcom.rc
-if [ -d /system_root ]; then
- ui_print "Use System-On-Root...";
- cp sbin/busybox /system_root/sbin
- chmod 755 /system_root/sbin/busybox
- backup_file /system/vendor/etc/init/hw/init.qcom.rc;
- remove_line /system/vendor/etc/init/hw/init.qcom.rc "start mpdecision";
- insert_line /system/vendor/etc/init/hw/init.qcom.rc "u:r:supersu:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:supersu:s0 root root -- /init.Ares.sh"
- insert_line /system/vendor/etc/init/hw/init.qcom.rc "u:r:magisk:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:magisk:s0 root root -- /init.Ares.sh"
- insert_line /system/vendor/etc/init/hw/init.qcom.rc "u:r:su:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:su:s0 root root -- /init.Ares.sh"
- insert_line /system/vendor/etc/init/hw/init.qcom.rc "u:r:init:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:init:s0 root root -- /init.Ares.sh"
- insert_line /system/vendor/etc/init/hw/init.qcom.rc "u:r:supersu:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:supersu:s0 root root -- /init.Ares.sh"
- insert_line /system/vendor/etc/init/hw/init.qcom.rc "root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:supersu:s0 root root -- /init.Ares.sh"
- insert_line /system/vendor/etc/init/hw/init.qcom.rc "Execute Ares boot script..." after "Post boot services" "    # Execute Ares boot script..."
- replace_string /system/vendor/etc/init/hw/init.qcom.rc "setprop sys.io.scheduler zen" "setprop sys.io.scheduler bfq" "setprop sys.io.scheduler zen";
-else
- backup_file init.qcom.rc;
- remove_line init.qcom.rc "start mpdecision";
- insert_line init.qcom.rc "u:r:supersu:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:supersu:s0 root root -- /init.Ares.sh"
- insert_line init.qcom.rc "u:r:magisk:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:magisk:s0 root root -- /init.Ares.sh"
- insert_line init.qcom.rc "u:r:su:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:su:s0 root root -- /init.Ares.sh"
- insert_line init.qcom.rc "u:r:init:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:init:s0 root root -- /init.Ares.sh"
- insert_line init.qcom.rc "u:r:supersu:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:supersu:s0 root root -- /init.Ares.sh"
- insert_line init.qcom.rc "root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:supersu:s0 root root -- /init.Ares.sh"
- insert_line init.qcom.rc "Execute Ares boot script..." after "Post boot services" "    # Execute Ares boot script..."
- replace_string init.qcom.rc "setprop sys.io.scheduler zen" "setprop sys.io.scheduler bfq" "setprop sys.io.scheduler zen";
-fi;
+backup_file init.qcom.rc;
+remove_line init.qcom.rc "start mpdecision";
+insert_line init.qcom.rc "u:r:supersu:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:supersu:s0 root root -- /init.Ares.sh"
+insert_line init.qcom.rc "u:r:magisk:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:magisk:s0 root root -- /init.Ares.sh"
+insert_line init.qcom.rc "u:r:su:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:su:s0 root root -- /init.Ares.sh"
+insert_line init.qcom.rc "u:r:init:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:init:s0 root root -- /init.Ares.sh"
+insert_line init.qcom.rc "u:r:supersu:s0 root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:supersu:s0 root root -- /init.Ares.sh"
+insert_line init.qcom.rc "root root -- /init.Ares.sh" after "Post boot services" "    exec u:r:supersu:s0 root root -- /init.Ares.sh"
+insert_line init.qcom.rc "Execute Ares boot script..." after "Post boot services" "    # Execute Ares boot script..."
+replace_string init.qcom.rc "setprop sys.io.scheduler zen" "setprop sys.io.scheduler bfq" "setprop sys.io.scheduler zen";
+
+# init.d support
+mount -o remount,rw -t auto /system
+backup_file /system/bin/sysinit
+backup_file /system/xbin/sysinit
+set perm 0 2000 755 /system/bin/sysinit~
+set perm 0 2000 755 /system/xbin/sysinit~
+
+mount -o remount,rw -t auto /system
+set perm 0 0 755 /system/etc/init/init_d.rc
+backup_file /system/etc/init/init_d.rc
+cp /system/etc/init/init_d.rc /system/etc/init/init_d.rc~
+set perm 0 0 644 /system/etc/init/init_d.rc~
+chmod +R 644 /system/etc/init/init_d.rc~
+chown +R root:root /system/etc/init/init_d.rc~
+
+set perm 0 0 755 /system/etc/init/init_d.rc;
+replace_file /system/etc/init/init_d.rc 755 init_d.rc
+replace_file /system/bin/sysinit 755 aressysinit
 
 # end ramdisk changes
 write_boot;
 
 ## end install
+
